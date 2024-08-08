d6e0dae4222417c51c5c27b661d06547072c7952
MadhuKumar-98 <madhu.s.puppala@98thpercentile.com>
2024-08-08 16:14:41 +0530

    added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome madhu kumar!</ThemedText>
+        <ThemedText type="title">Welcome madhu Kumar!</ThemedText>
         
         <HelloWave />
       </ThemedView>
